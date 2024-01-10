95bc99845791087f5fe5d77e409adaba50ad3a4d
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2024-01-10 17:06:26 +0200

    All new

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Product } from "./Product";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Best selling</h1>
+
+      <Product name="Tacos With Lime" />
+      <Product name="Fries and Burger" />
+    </div>
+  );
+}
