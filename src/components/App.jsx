b40acddc9796af7ffc4f7ab273b2d3c0287ba5bb
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2024-01-16 15:09:03 +0200

    some fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,10 +11,14 @@ export const App = () => {
       <BookList books={favouriteBooks} />
       <HiOutlineArrowLongDown />
       <Alert variont="good">All good!</Alert>
+      <App1 />
       <Alert variont="warning">
         Payment received, thank you for your purchase!
       </Alert>
+      <CustomButton message="Playing music!">Play some music</CustomButton>
+      <CustomButton message="Uploading your data!">Upload data</CustomButton>
       <Alert variont="error">There was an error during transaction!</Alert>
+      <App2 />
     </>
   );
 };
@@ -27,6 +31,8 @@ export const App1 = () => {
   return <button onClick={handleClick}>Click me!</button>;
 };
 
+// -------------------
+
 export const App2 = () => {
   const handleClick = (evt) => {
     console.log(evt);
@@ -38,4 +44,10 @@ export const App2 = () => {
       <button onClick={(evt) => console.log(evt)}>Second button</button>
     </>
   );
+};
+
+// ----------------------
+
+export const CustomButton = ({ message, children }) => {
+  return <button onClick={() => alert(message)}>{children}</button>;
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App, App1, App2 } from "./components/App.jsx";
+import { App } from "./components/App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-    <App1 />
-    <App2 />
   </React.StrictMode>
 );
