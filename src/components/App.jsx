b150e70d81b642ee16a73812b167ab419338900c
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2024-01-16 13:50:05 +0200

    add boutton

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,15 @@
 import "./App.css";
-// import { favouriteBooks } from "./BookList";
-// import { Product } from "./Product";
 import { BookList } from "./BookList";
 import { favouriteBooks } from "./Server";
 import { Alert } from "./Alert";
+import { HiOutlineArrowLongDown } from "react-icons/hi2";
 
 export const App = () => {
   return (
     <>
       <h1>Books of the week</h1>
       <BookList books={favouriteBooks} />
+      <HiOutlineArrowLongDown />
       <Alert variont="good">All good!</Alert>
       <Alert variont="warning">
         Payment received, thank you for your purchase!
@@ -17,4 +17,12 @@ export const App = () => {
       <Alert variont="error">There was an error during transaction!</Alert>
     </>
   );
+};
+
+export const App1 = () => {
+  const handleClick = () => {
+    alert("I'm a button!");
+  };
+
+  return <button onClick={handleClick}>Click me!</button>;
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./components/App.jsx";
+import { App, App1 } from "./components/App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <App1 />
   </React.StrictMode>
 );
